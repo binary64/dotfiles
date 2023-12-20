27dbbd9c01d4cdfdc78d09520abf31d2afb66ed7
binary64 <1680627+binary64@users.noreply.github.com>
2023-12-20 14:48:12 +0000

    add the remaining scripts

--- install.sh
+++ install.sh
@@ -106,10 +106,103 @@ ls /dev/disk/by-partlabel/cryptroot >/dev/null
 ## format the EFI partition
 mkfs.vfat /dev/disk/by-partlabel/efiboot
 
+# temporary keyfile, will be removed (8k, ridiculously large)
+dd if=/dev/urandom of=/tmp/keyfile bs=1k count=8
+
+## now we encrypt the second partition
+# pick a strong passphrase
+
+echo "Creating the encrypted partition, follow the instructions and use a strong password!"
+# formats the partition with luks and adds the temporary keyfile.
+echo "YES" | cryptsetup luksFormat /dev/disk/by-partlabel/cryptroot --key-size 512 --hash sha512 --key-file /tmp/keyfile
+
+if [[ $use_passphrase != "no" ]];
+then
+	# sets the given passphrase or asks for one
+	if [[ "${passphrase}" != "NONE" ]];
+	then
+		echo "$passphrase" | cryptsetup luksAddKey /dev/disk/by-partlabel/cryptroot --key-file /tmp/keyfile
+	else
+		cryptsetup luksAddKey /dev/disk/by-partlabel/cryptroot --key-file /tmp/keyfile
+	fi
+	echo "added passphrase"
+fi
 
 
-bash formatluks.sh
-bash formatzfs.sh
+if [[ "${keyfile}" != "NONE" ]];
+then
+	cryptsetup luksAddKey /dev/disk/by-partlabel/cryptroot  -d /tmp/keyfile --new-keyfile-size="${keysize}" "${keyfile}"
+	echo "added keyfile"
+fi
+
+
+# mount the cryptdisk at /dev/mapper/nixroot
+cryptsetup luksOpen /dev/disk/by-partlabel/cryptroot nixroot -d /tmp/keyfile
+
+# remove the temporary keyfile
+cryptsetup luksRemoveKey /dev/disk/by-partlabel/cryptroot /tmp/keyfile
+rm -f /tmp/keyfile
+
+## the actual zpool create is below
+#
+# zpool create        \
+# -O atime=on         \ #
+# -O relatime=on      \ # only write access time (requires atime, see man zfs)
+# -O compression=lz4  \ # compress all the things! (man zfs)
+# -O snapdir=visible  \ # ever so sligthly easier snap management (man zfs)
+# -O xattr=sa         \ # selinux file permissions (man zfs)
+# -o ashift=12        \ # 4k blocks (man zpool)
+# -o altroot=/mnt     \ # temp mount during install (man zpool)
+# rpool               \ # new name of the pool
+# /dev/mapper/nixroot   # devices used in the pool (in my case one, so no mirror or raid)
+
+zpool create        \
+-O atime=on         \
+-O relatime=on      \
+-O compression=lz4  \
+-O snapdir=visible  \
+-O xattr=sa         \
+-o ashift=12        \
+-o altroot=/mnt     \
+rpool               \
+/dev/mapper/nixroot \
+
+
+# dataset for / (root)
+zfs create -o mountpoint=none rpool/root
+echo created root dataset
+zfs create -o mountpoint=legacy rpool/root/nixos
+
+# dataset for home, make copies of all files against corruption
+zfs create -o copies=2 -o mountpoint=legacy rpool/home
+
+# dataset for swap
+zfs create -o compression=off -V 8G rpool/swap
+mkswap -L SWAP /dev/zvol/rpool/swap
+swapon /dev/zvol/rpool/swap
+
+# mount the root dataset at /mnt
+mount -t zfs rpool/root/nixos /mnt
+
+# mount the home datset at future /home
+mkdir -p /mnt/home
+mount -t zfs rpool/home /mnt/home
+
+# mount EFI partition at future /boot
+mkdir -p /mnt/boot
+mount  /dev/disk/by-partlabel/efiboot /mnt/boot
+
+# set boot filesystem
+zpool set bootfs=rpool/root/nixos rpool
+
+# enable auto snapshots for home dataset
+# defaults to keeping:
+# - 4 frequent snapshots (1 per 15m)
+# - 24 hourly snapshots
+# - 7 daily snapshots 
+# - 4 weekly snapshots 
+# - 12 monthly snapshots
+zfs set com.sun:auto-snapshot=true rpool/home
 
 nixos-generate-config --root /mnt
 
