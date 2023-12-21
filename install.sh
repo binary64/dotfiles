0e7ec0957fc8f9498078fa7e617db23cb303c4c8
binary64 <1680627+binary64@users.noreply.github.com>
2023-12-21 00:49:37 +0000

    use variable for part_root

--- install.sh
+++ install.sh
@@ -123,18 +123,18 @@ dd if=/dev/urandom of=/tmp/keyfile bs=1k count=8
 
 echo "Creating the encrypted partition, follow the instructions and use a strong password!"
 # formats the partition with luks and adds the temporary keyfile.
-echo "YES" | cryptsetup luksFormat /dev/disk/by-partlabel/cryptroot --key-size 512 --hash sha512 --key-file /tmp/keyfile
+echo "YES" | cryptsetup luksFormat /dev/disk/by-partlabel/${PART_ROOT} --key-size 512 --hash sha512 --key-file /tmp/keyfile
 echo "Encryption setup."
 
-echo "$passphrase" | cryptsetup luksAddKey /dev/disk/by-partlabel/cryptroot --key-file /tmp/keyfile
+echo "$passphrase" | cryptsetup luksAddKey /dev/disk/by-partlabel/${PART_ROOT} --key-file /tmp/keyfile
 echo "added passphrase"
 
 # mount the cryptdisk at /dev/mapper/nixroot
-cryptsetup luksOpen /dev/disk/by-partlabel/cryptroot nixroot -d /tmp/keyfile
+cryptsetup luksOpen /dev/disk/by-partlabel/${PART_ROOT} nixroot -d /tmp/keyfile
 echo "Disk mounted."
 
 # remove the temporary keyfile
-cryptsetup luksRemoveKey /dev/disk/by-partlabel/cryptroot /tmp/keyfile
+cryptsetup luksRemoveKey /dev/disk/by-partlabel/${PART_ROOT} /tmp/keyfile
 rm -f /tmp/keyfile
 
 zpool create        \
