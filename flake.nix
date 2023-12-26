3a3856bafb684831a8473cf15ed267e6dcde2d49
binary64 <1680627+binary64@users.noreply.github.com>
2023-12-26 22:56:51 +0000

    update hm flake

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       stable.url = "github:NixOS/nixpkgs/nixos-23.11";
       nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
       home-manager = {
-        url = "github:nix-community/home-manager/release-23.11";
+        url = "github:nix-community/home-manager";
         inputs.nixpkgs.follows = "nixpkgs";
       };
       agenix.url = "github:ryantm/agenix";
