abc7669b2ba46704d6fd97ed72ab14d1810f7f37
binary64 <1680627+binary64@users.noreply.github.com>
2023-12-27 23:42:45 +0000

    23.11

--- default.nix
+++ default.nix
@@ -41,7 +41,7 @@ with lib.my;
       };
     };
   system.configurationRevision = with inputs; mkIf (self ? rev) self.rev;
-  system.stateVersion = "22.11";
+  system.stateVersion = "23.11";
 
   ## Some reasonable, global defaults
   # This is here to appease 'nix flake check' for generic hosts with no
