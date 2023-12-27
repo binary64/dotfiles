2155c577d4bc912964229d20c55fddc806801d5d
binary64 <1680627+binary64@users.noreply.github.com>
2023-12-27 23:27:58 +0000

    fix hyprland import

--- home-manager/desktop/hyprland.nix
+++ home-manager/desktop/hyprland.nix
@@ -1,10 +1,10 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 {
   imports = [
-    hyprland.homeManagerModules.default
+    inputs.hyprland.homeManagerModules.default
   ];
-  
+
   home = {
     sessionVariables = {
         EDITOR = "lvim";
