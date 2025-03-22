a7b238e9b55d84888c512808ee614f5e8fac6f07
binary64 <1680627+binary64@users.noreply.github.com>
2025-03-22 17:13:16 +0000

    trying out zfs

--- hosts/home.nix
+++ hosts/home.nix
@@ -19,9 +19,9 @@ with lib; {
     #   neovim = {
     #     enable = true;
     #   };
-    #   hyprland = {
-    #     enable = true;
-    #   };
+      hyprland = {
+        enable = true;
+      };
   };
   #environment.systemPackages = with pkgs; [
   #  nodePackages.pnpm
@@ -29,6 +29,44 @@ with lib; {
 
   #programs.steam.enable = true;
 
+  # set a super secure initial root password
+  users.users.root.initialHashedPassword = "$6$nAffBk9Fxs.0J13A$S4e5cCSd.ITeYAZydUnfwo6eHXiYJuzbp3RPKHf8xtnP25V1Zk0eypKFeg0LXDTnJsfRv5O21TKMavcb3c9qE1"
+
   # better for steam proton games
   systemd.extraConfig = "DefaultLimitNOFILE=1048576";
+
+  boot.supportedFilesystems = [ "zfs" ];
+  networking.hostId = "$(head -c 8 /etc/machine-id)";
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.zfs.devNodes = "/dev/disk/by-partlabel";
+
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.canTouchEfiVariables = false;
+  boot.loader.generationsDir.copyKernels = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.copyKernels = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.zfsSupport = true;
+
+  boot.loader.grub.extraPrepareConfig = ''
+    mkdir -p /boot/efis
+    for i in  /boot/efis/*; do mount $i ; done
+
+    mkdir -p /boot/efi
+    mount /boot/efi
+  '';
+
+  boot.loader.grub.extraInstallCommands = ''
+    ESP_MIRROR=$(mktemp -d)
+    cp -r /boot/efi/EFI $ESP_MIRROR
+    for i in /boot/efis/*; do
+      cp -r $ESP_MIRROR/EFI $i
+    done
+    rm -rf $ESP_MIRROR
+  '';
+
+  boot.loader.grub.devices = [
+    /dev/sda
+  ];
 }
--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/bin/env bash
 
+###############################################################################
+#    nixos-zfs-setup.sh: a bash script that sets up zfs disks for NixOS.      #
+#      Copyright (C) 2022  Mark (voidzero) van Dijk, The Netherlands.         #
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.   #
+###############################################################################
+
 set -euo pipefail
 
 # Check for existence of `sudo` binary
@@ -26,11 +44,129 @@ if ! command -v zpool &>/dev/null; then
 	exit 1
 fi
 
-echo "Partitioning disk with disko.."
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko --flake ~/dotfiles#desktop
+DISK=(/dev/sda)
+echo "Partitioning zfs onto ${DISK} using sgdisk.."
+
+# How to name the partitions. This will be visible in 'gdisk -l /dev/disk' and
+# in /dev/disk/by-partlabel.
+PART_MBR="bootcode"
+PART_EFI="efiboot"
+PART_BOOT="bpool"
+PART_SWAP="swap"
+PART_ROOT="rpool"
+
+# How much swap per disk?
+SWAPSIZE=2G
+
+# The type of virtual device for boot and root. If kept empty, the disks will
+# be joined (two 1T disks will give you ~2TB of data). Other valid options are
+# mirror, raidz types and draid types. You will have to manually add other
+# devices like spares, intent log devices, cache and so on. Here we're just
+# setting this up for installation after all.
+#ZFS_BOOT_VDEV="mirror"
+#ZFS_ROOT_VDEV="mirror"
+
+# How to name the boot pool and root pool.
+ZFS_BOOT="bpool"
+ZFS_ROOT="rpool"
+
+# How to name the root volume in which nixos will be installed.
+# If ZFS_ROOT is set to "rpool" and ZFS_ROOT_VOL is set to "nixos",
+# nixos will be installed in rpool/nixos, with a few extra subvolumes
+# (datasets).
+ZFS_ROOT_VOL="nixos"
+
+i=0 SWAPDEVS=()
+for d in ${DISK[*]}; do
+	sgdisk --zap-all "${d}"
+	sgdisk -a1 -n1:0:+100K -t1:EF02 -c 1:${PART_MBR}${i} "${d}"
+	sgdisk -n2:1M:+1G -t2:EF00 -c 2:${PART_EFI}${i} "${d}"
+	sgdisk -n3:0:+4G -t3:BE00 -c 3:${PART_BOOT}${i} "${d}"
+	sgdisk -n4:0:+${SWAPSIZE} -t4:8200 -c 4:${PART_SWAP}${i} "${d}"
+	SWAPDEVS+=(${d}4)
+	sgdisk -n5:0:0 -t5:BF00 -c 5:${PART_ROOT}${i} "${d}"
+
+	partprobe "${d}"
+	sleep 2
+	mkswap -L ${PART_SWAP}fs${i} /dev/disk/by-partlabel/${PART_SWAP}${i}
+	swapon /dev/disk/by-partlabel/${PART_SWAP}${i}
+	((i++)) || true
+done
+unset i d
+
+# Wait for a bit to let udev catch up and generate /dev/disk/by-partlabel.
+sleep 6s
+
+# Create the boot pool
+zpool create \
+	-o compatibility=grub2 \
+	-o ashift=12 \
+	-o autotrim=on \
+	-O acltype=posixacl \
+	-O compression=lz4 \
+	-O devices=off \
+	-O normalization=formD \
+	-O relatime=on \
+	-O xattr=sa \
+	-O mountpoint=none \
+	-O checksum=sha256 \
+	-R /mnt \
+	${ZFS_BOOT} "${ZFS_BOOT_VDEV}" /dev/disk/by-partlabel/${PART_BOOT}*
+
+# Create the root pool
+zpool create \
+	-o ashift=12 \
+	-o autotrim=on \
+	-O acltype=posixacl \
+	-O compression=zstd \
+	-O dnodesize=auto -O normalization=formD \
+	-O relatime=on \
+	-O xattr=sa \
+	-O mountpoint=none \
+	-O checksum=edonr \
+	-R /mnt \
+	${ZFS_ROOT} "${ZFS_ROOT_VDEV}" /dev/disk/by-partlabel/${PART_ROOT}*
+
+# Create the boot dataset
+zfs create ${ZFS_BOOT}/${ZFS_ROOT_VOL}
+
+# Create the root dataset
+zfs create -o mountpoint=/ ${ZFS_ROOT}/${ZFS_ROOT_VOL}
+
+# Create datasets (subvolumes) in the root dataset
+zfs create ${ZFS_ROOT}/${ZFS_ROOT_VOL}/home
+zfs create -o atime=off ${ZFS_ROOT}/${ZFS_ROOT_VOL}/nix
+zfs create ${ZFS_ROOT}/${ZFS_ROOT_VOL}/root
+zfs create ${ZFS_ROOT}/${ZFS_ROOT_VOL}/usr
+zfs create ${ZFS_ROOT}/${ZFS_ROOT_VOL}/var
+
+# Create datasets (subvolumes) in the boot dataset
+# This comes last because boot order matters
+zfs create -o mountpoint=/boot ${ZFS_BOOT}/${ZFS_ROOT_VOL}/boot
+
+# Create, mount and populate the efi partitions
+i=0
+for d in ${DISK[*]}; do
+	mkfs.vfat -n EFI /dev/disk/by-partlabel/${PART_EFI}${i}
+	mkdir -p /mnt/boot/efis/${PART_EFI}${i}
+	mount -t vfat /dev/disk/by-partlabel/${PART_EFI}${i} /mnt/boot/efis/${PART_EFI}${i}
+	((i++)) || true
+done
+unset i d
+
+# Mount the first drive's efi partition to /mnt/boot/efi
+mkdir /mnt/boot/efi
+mount -t vfat /dev/disk/by-partlabel/${PART_EFI}0 /mnt/boot/efi
+
+# Make sure we won't trip over zpool.cache later
+mkdir -p /mnt/etc/zfs/
+rm -f /mnt/etc/zfs/zpool.cache
+touch /mnt/etc/zfs/zpool.cache
+chmod a-w /mnt/etc/zfs/zpool.cache
+chattr +i /mnt/etc/zfs/zpool.cache
 
 echo "Installing OS.."
-sudo nixos-install --flake ~/dotfiles#desktop --impure --root /mnt --no-root-passwd
+nixos-generate-config --root /mnt
 
 # Ensure /mnt directory exists
 if [ ! -d /mnt ]; then
@@ -38,7 +174,6 @@ if [ ! -d /mnt ]; then
 	exit 1
 fi
 
-cp -r ~/dotfiles /mnt/etc/nixos
+cp -r ./* /mnt/etc/nixos
 lsblk
-
-echo "DONE! You should now \`reboot\` into your new system."
+ls -al /mnt
