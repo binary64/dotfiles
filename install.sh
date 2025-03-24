fb05be70f8c3f4878bc3a754943a753a93875fb0
binary64 <1680627+binary64@users.noreply.github.com>
2025-03-24 06:44:03 +0000

    relative path

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin bash
 set -xeuo pipefail
 
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount /etc/nixos/hosts/desktop/disko.nix
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount hosts/desktop/disko.nix
 
 nixos-install --show-trace --no-root-passwd --root /mnt
 
