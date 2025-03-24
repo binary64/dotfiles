910a3d19c259c7b0917bd814a3dfb16431bee63b
binary64 <1680627+binary64@users.noreply.github.com>
2025-03-24 07:47:02 +0000

    debugging

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@ set -xeuo pipefail
 
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount --yes-wipe-all-disks hosts/desktop/disko.nix
 
+ls -al /mnt || true
+ls -al /mnt/boot || true
+
+sudo mount -t vfat /dev/disk/by-partlabel/disk-vda-ESP /mnt/boot
+
+ls -al /mnt || true
+ls -al /mnt/boot || true
+
 mkdir -p /mnt/home/etc/nixos
 cp -r hosts/desktop/* /mnt/home/etc/nixos
 
