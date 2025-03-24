4cc509d851d1162b301709aba4e06ca191129970
binary64 <1680627+binary64@users.noreply.github.com>
2025-03-24 06:58:10 +0000

    try https://discourse.nixos.org/t/disko-zfs-cannot-import-zroot-no-such-pool-available/36990/3

--- hosts/desktop/disko.nix
+++ hosts/desktop/disko.nix
@@ -1,39 +1,16 @@
+{ disks ? [ "/dev/sda" ], ... }:
 {
-  # Grub Configuration with plymouth
-  boot = {
-    consoleLogLevel = 0;
-    kernelParams = [
-      "quiet"
-      "udev.log_level=3"
-    ];
-    initrd = {
-      systemd = {
-        enable = true;
-      };
-      verbose = false;
-    };
-    loader = {
-      grub = {
-        enable = true;
-        configurationLimit = 10;
-      };
-    };
-  };
   disko.devices = {
     disk = {
-      main = {
+      vda = {
         type = "disk";
-        device = "/dev/disk/by-id/ata-Samsung_SSD_840_EVO_120GB_S1D7NEAF411408R";
+        device = builtins.elemAt disks 0;
         content = {
           type = "gpt";
           partitions = {
-            GRUB = {
-              size = "1M";
-              type = "EF02";
-            };
-            BOOT = {
-              size = "512M";
-              type = "0700";
+            ESP = {
+              size = "1G";
+              type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
@@ -41,17 +18,10 @@
               };
             };
             zfs = {
-              end = "-8G";
-              content = {
-                type = "zfs";
-                pool = "rpool";
-              };
-            };
-            encryptedSwap = {
               size = "100%";
               content = {
-                type = "swap";
-                randomEncryption = true;
+                type = "zfs";
+                pool = "zroot";
               };
             };
           };
@@ -59,77 +29,20 @@
       };
     };
     zpool = {
-      rpool = {
+      zroot = {
         type = "zpool";
-        mode = "";
-        options = {
-          ashift = "12";
-          autotrim = "on";
-        };
-        rootFsOptions = {
-          acltype = "posixacl";
-          canmount = "off";
-          compression = "lz4";
-          devices = "off";
-          dnodesize = "auto";
-          encryption = "on";
-          keyformat = "passphrase";
-          keylocation = "prompt";
-          mountpoint = "none";
-          normalization = "formD";
-          relatime = "on";
-          xattr = "sa";
-        };
+
         datasets = {
-          "nixos" = {
-            type = "zfs_fs";
-            options.mountpoint = "none";
-          };
-          "nixos/nix" = {
-            type = "zfs_fs";
-            options.mountpoint = "legacy";
-            mountpoint = "/nix";
-          };
-          "nixos/root" = {
+          root = {
             type = "zfs_fs";
             options.mountpoint = "legacy";
             mountpoint = "/";
+            options."com.sun:auto-snapshot" = "false";
           };
-          "nixos/root/var" = {
+          home = {
             type = "zfs_fs";
-            options.mountpoint = "none";
-          };
-          "nixos/root/var/lib" = {
-            type = "zfs_fs";
-            options.mountpoint = "legacy";
-            mountpoint = "/var/lib";
-          };
-          "nixos/root/var/lib/containers" = {
-            type = "zfs_fs";
-            options.mountpoint = "none";
-          };
-          "nixos/root/var/lib/containers/storage" = {
-            type = "zfs_fs";
-            options.mountpoint = "none";
-          };
-          "nixos/root/var/lib/containers/storage/volumes" = {
-            type = "zfs_fs";
-            options.mountpoint = "legacy";
-            mountpoint = "/var/lib/containers/storage/volumes";
-          };
-          "data" = {
-            type = "zfs_fs";
-            options.mountpoint = "none";
-          };
-          "data/home" = {
-            type = "zfs_fs";
-            options.mountpoint = "legacy";
+	          options.mountpoint = "legacy";
             mountpoint = "/home";
-          };
-          "reserved" = {
-            type = "zfs_fs";
-            options.mountpoint = "none";
-            options.refreservation = "10G";
           };
         };
       };
