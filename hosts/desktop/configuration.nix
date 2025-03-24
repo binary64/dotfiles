e3775e2f9a731bcb599666c01beaead9b439b455
binary64 <1680627+binary64@users.noreply.github.com>
2025-03-24 07:24:43 +0000

    no autodisco

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -9,10 +9,7 @@
 {
   imports = [
     # Include the results of the hardware scan.
-    ./disko.nix
     ./hardware-configuration.nix
-
-    inputs.disko.nixosModules.disko
   ];
 
   networking.hostName = "desktop";
