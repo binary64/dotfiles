658ff8627aa756747a45bfe60409b144fa6f0a24
binary64 <1680627+binary64@users.noreply.github.com>
2025-03-25 13:34:45 +0000

    fix boot paths

--- hosts/desktop/zfs.nix
+++ hosts/desktop/zfs.nix
@@ -5,36 +5,35 @@
   networking.hostId = "d1a52b06";
   boot.kernelPackages = pkgs.linuxPackages_6_6;
   boot.zfs.devNodes = "/dev/disk/by-partlabel";
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  
+  boot.loader.efi.efiSysMountPoint = "/boot";
   boot.loader.efi.canTouchEfiVariables = false;
   boot.loader.generationsDir.copyKernels = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
+  
+  # GRUB configuration
   boot.loader.grub.enable = true;
   boot.loader.grub.copyKernels = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.zfsSupport = true;
-
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.devices = [ "/dev/sda" ];
+  
   boot.loader.grub.extraPrepareConfig = ''
-    mkdir -p /boot/efis
-    for i in  /boot/efis/*; do mount $i ; done
-
-    mkdir -p /boot/efi
-    mount /boot/efi
+    mkdir -p /boot
+    mount /dev/disk/by-label/BOOT /boot
   '';
 
   boot.loader.grub.extraInstallCommands = ''
-    ESP_MIRROR=$(${pkgs.coreutils}/bin/mktemp -d)
-    ${pkgs.coreutils}/bin/cp -r /boot/efi/EFI $ESP_MIRROR
-    for i in /boot/efis/*; do
-      ${pkgs.coreutils}/bin/cp -r $ESP_MIRROR/EFI $i
-    done
-    ${pkgs.coreutils}/bin/rm -rf $ESP_MIRROR
+    ${pkgs.coreutils}/bin/mkdir -p /boot/EFI
   '';
 
-  boot.loader.grub.devices = [
-    "/dev/sda"
-  ];
+  # Initial root password
+  users.users.root.initialPassword = "123";
 
-users.users.root.initialPassword = "123";
-
+  # Ensure the boot partition is mounted early
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/BOOT";
+    fsType = "vfat";
+    options = [ "nofail" ];
+  };
 }
