ee271cb6d51b2c89c68e9ebb955a8e3095bce7a7
binary64 <1680627+binary64@users.noreply.github.com>
2025-03-25 11:47:00 +0000

    install viper's config again

--- flake.nix
+++ flake.nix
@@ -1,22 +1,106 @@
 {
-  description = "A simple NixOS flake";
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      {
+        lib,
+        config,
+        ...
+      }:
+      {
+        imports = [
+          ./packages
+          ./misc/lib
+          ./hosts
+        ];
+
+        flake = {
+          nixosModules = config.flake.lib.dirToAttrs ./modules/nixos;
+        };
+
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
+
+        perSystem =
+          {
+            pkgs,
+            config,
+            ...
+          }:
+          {
+            devShells.default =
+              with pkgs;
+              mkShellNoCC {
+                packages = [
+                  lua-language-server
+                  config.packages.stylua
+                  taplo
+                ];
+              };
+          };
+      }
+    );
 
   inputs = {
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    imports = [ ./home.nix ];
-
-    nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./hosts/desktop/configuration.nix
-      ];
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
     };
-    
+
+    nix-common = {
+      url = "github:viperML/nix-common";
+    };
+    nh = {
+      url = "github:viperML/nh";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hover-rs = {
+      url = "github:viperML/hover-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    noshell = {
+      url = "github:viperML/noshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    wrapper-manager = {
+      url = "github:viperML/wrapper-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    guix-search = {
+      url = "github:viperML/guix-search";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    activation-manager = {
+      url = "github:viperML/activation-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+
+    nix-index-database = {
+      url = "github:Mic92/nix-index-database";
+    };
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.4.2";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+    };
+    nil = {
+      url = "github:oxalica/nil";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 }
