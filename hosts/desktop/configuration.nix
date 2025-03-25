94df851896a033142f0966b27eae511e75bab9de
binary64 <1680627+binary64@users.noreply.github.com>
2025-03-25 11:06:12 +0000

    fix configuration files

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -10,7 +10,7 @@
       ./hardware-configuration.nix ./zfs.nix
     ];
 
-  nix.settings.experimental-features = [ "flakes" ] ;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ] ;
 
   # Define on which hard drive you want to install Grub.
 
