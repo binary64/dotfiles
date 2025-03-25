209266628f2ce49e41e17b17af2165085ab0d0fa
binary64 <1680627+binary64@users.noreply.github.com>
2025-03-25 11:56:37 +0000

    @@@

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,1 +1,36 @@
-{}
+{
+  config,
+  withSystem,
+  mkNixos,
+  inputs,
+  ...
+}: let
+  system = "x86_64-linux";
+  inherit (config.flake) nixosModules;
+in {
+  flake.nixosConfigurations.desktop = withSystem system ({pkgs, ...}:
+    mkNixos system [
+      #-- Topology
+      ./configuration.nix
+      # nixosModules.es
+      # inputs.lanzaboote.nixosModules.lanzaboote
+      # nixosModules.tmpfs
+      # nixosModules.tpm2
+      # nixosModules.user-ayats
+      # nixosModules.user-soch
+      # nixosModules.yubikey
+
+      #-- Environment
+      {services.displayManager.autoLogin.user = "ayats";}
+      # nixosModules.plasma6
+      nixosModules.gnome
+
+      # #-- Other
+      # nixosModules.tailscale
+      # nixosModules.guix
+      # # nixosModules.docker
+      # nixosModules.printing
+      # # nixosModules.incus
+      # nixosModules.podman
+    ]);
+}
---
+++ modules/nixos/gnome.nix
@@ -0,0 +1,48 @@
+{ pkgs, ... }:
+{
+  services.xserver = {
+    displayManager.gdm.enable = true;
+    desktopManager.gnome = {
+      enable = true;
+      extraGSettingsOverridePackages = with pkgs; [
+        mutter
+      ];
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    adw-gtk3
+    gnome-tweaks
+    wl-clipboard
+    vanilla-dmz
+    gnomeExtensions.appindicator
+    gnomeExtensions.dash-to-panel
+  ];
+
+  environment.gnome.excludePackages = with pkgs; [
+    totem
+    gnome-maps
+    gnome-logs
+    simple-scan
+    gnome-calculator
+    epiphany
+    gnome-music
+    gnome-tour
+    gnome-contacts
+    gnome-disk-utility
+  ];
+
+  programs.geary.enable = false;
+
+  environment.sessionVariables = {
+    MOZ_ENABLE_WAYLAND = "1";
+    SSH_ASKPASS_REQUIRE = "prefer";
+    NIXOS_OZONE_WL = "1";
+    XCURSOR_THEME = "DMZ-White";
+    XCURSOR_SIZE = "240";
+  };
+
+  # programs.git.config = {
+  #   credential.helper = "${pkgs.gitFull}/bin/git-credential-libsecret";
+  # };
+}
