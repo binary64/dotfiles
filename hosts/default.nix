67d0322aeeb4fce97dd3c57fd3c5a8c31eb684d1
binary64 <1680627+binary64@users.noreply.github.com>
2025-03-25 21:03:44 +0000

    try uk keyMap

--- hosts/default.nix
+++ hosts/default.nix
@@ -52,7 +52,7 @@
             i18n.defaultLocale = "en_GB.UTF-8";
             console = {
               # font = "Lat2-Terminus16";
-              keyMap = lib.mkForce "gb";
+              keyMap = lib.mkForce "uk";
               useXkbConfig = true; # use xkb.options in tty.
             };
           }
