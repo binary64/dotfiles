7b82e8b3ac0135269b0309a4185e6243c11d6210
binary64 <1680627+binary64@users.noreply.github.com>
2025-03-25 09:59:55 +0000

    @@@

---
+++ zfs.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+
+{
+  boot.supportedFilesystems = [ "zfs" ];
+  networking.hostId = "d1a52b06";
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.zfs.devNodes = "/dev/disk/by-partlabel";
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.canTouchEfiVariables = false;
+  boot.loader.generationsDir.copyKernels = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.copyKernels = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.zfsSupport = true;
+
+  boot.loader.grub.extraPrepareConfig = ''
+    mkdir -p /boot/efis
+    for i in  /boot/efis/*; do mount $i ; done
+
+    mkdir -p /boot/efi
+    mount /boot/efi
+  '';
+
+  boot.loader.grub.extraInstallCommands = ''
+    ESP_MIRROR=$(${pkgs.coreutils}/bin/mktemp -d)
+    ${pkgs.coreutils}/bin/cp -r /boot/efi/EFI $ESP_MIRROR
+    for i in /boot/efis/*; do
+      ${pkgs.coreutils}/bin/cp -r $ESP_MIRROR/EFI $i
+    done
+    ${pkgs.coreutils}/bin/rm -rf $ESP_MIRROR
+  '';
+
+  boot.loader.grub.devices = [
+    "/dev/sda"
+  ];
+
+users.users.root.initialPassword = "123";
+
+}
