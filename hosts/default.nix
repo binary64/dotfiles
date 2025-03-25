df1c555e1b2a1a3aee7059b17559a9bf7f1515ab
binary64 <1680627+binary64@users.noreply.github.com>
2025-03-25 10:05:37 +0000

    @@@

---
+++ hosts/default.nix
@@ -0,0 +1,52 @@
+{
+  inputs,
+  config,
+  withSystem,
+  ...
+}:
+{
+  imports = [
+    ./desktop
+  ];
+
+  _module.args.mkNixos =
+    system: extraModules:
+    let
+      specialArgs = withSystem system (
+        {
+          inputs',
+          self',
+          ...
+        }:
+        {
+          inherit self' inputs' inputs;
+        }
+      );
+    in
+    inputs.nixpkgs.lib.nixosSystem {
+      inherit specialArgs;
+
+      modules = [
+        #-- Core
+        inputs.nixpkgs.nixosModules.readOnlyPkgs
+        { nixpkgs.pkgs = withSystem system ({ pkgs, ... }: pkgs); }
+
+        config.flake.nixosModules.common
+        inputs.nix-common.nixosModules.default
+
+        inputs.noshell.nixosModules.default
+        # { programs.noshell.enable = true; }
+
+        inputs.activation-manager.nixosModules.home
+
+        (
+          { config, ... }:
+          {
+            users.users.ayats.packages = [
+              (config.activation-manager.mkHome ../modules/activation-manager/main.nix)
+            ];
+          }
+        )
+      ] ++ extraModules;
+    };
+}
