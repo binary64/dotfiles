bcbec05858af3c85519dad9c3175fc63735d3032
binary64 <1680627+binary64@users.noreply.github.com>
2025-03-25 09:53:53 +0000

    new repo! from scratch

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      {
+        lib,
+        config,
+        ...
+      }:
+      {
+        imports = [
+          ./packages
+          ./misc/lib
+          ./hosts
+        ];
+
+        flake = {
+          nixosModules = config.flake.lib.dirToAttrs ./modules/nixos;
+        };
+
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
+
+        perSystem =
+          {
+            pkgs,
+            config,
+            ...
+          }:
+          {
+            devShells.default =
+              with pkgs;
+              mkShellNoCC {
+                packages = [
+                  lua-language-server
+                  config.packages.stylua
+                  taplo
+                ];
+              };
+          };
+      }
+    );
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    
+  };
+}
